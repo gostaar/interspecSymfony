77468d4ea839cf315346494079910b78525454e3
Delphine <bauwens.delphine@gmail.com>
2023-05-16 10:57:56 +0200

    vich bundle

--- src/Controller/Admin/LivreCrudController.php
+++ src/Controller/Admin/LivreCrudController.php
@@ -3,8 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Livre;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class LivreCrudController extends AbstractCrudController
 {
@@ -16,7 +21,14 @@ class LivreCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
-        yield from parent::configureFields($pageName);
+        yield TextField::new('titre');
+        yield TextField::new('isbn');
+        yield TextField::new('description');
+
+        yield BooleanField::new('archive');
+
+        yield TextareaField::new('imageFile')->setFormType(VichImageType::class);
+
         yield AssociationField::new('auteur');
         yield AssociationField::new('editeur');
         yield AssociationField::new('genres');
